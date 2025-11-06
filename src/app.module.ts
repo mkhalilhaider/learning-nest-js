106d66e04e67b3610e35f5d7b95496e4763658ac
mkhalilhaider <khalil.haider2109@gmail.com>
2025-11-06 10:45:00 +0500

    implement basic AuthGuard for product endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { CustomerModule } from './customer/customer.module';
 import { MynameController } from './myname/myname.controller';
+import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
 
 @Module({
   imports: [StudentModule, CustomerModule],
-  controllers: [AppController, MynameController],
-  providers: [AppService],
+  controllers: [AppController, MynameController, ProductController],
+  providers: [AppService, ProductService],
 })
 export class AppModule {}
---
+++ src/product/product.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+
+@Controller('product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  @UseGuards(AuthGuard)
+  getProducts() {
+    return this.productService.getAllProducts();
+  }
+  @Get(':id')
+  @UseGuards(AuthGuard)
+  getProduct(@Param('id') id: string) {
+    return this.productService.getProductById(Number(id));
+  }
+}
