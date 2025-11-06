118ea2423985ab11e0579b56f5613ac54583b964
mkhalilhaider <khalil.haider2109@gmail.com>
2025-11-06 10:10:42 +0500

    implemented custom pipe to capitalize name

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { CustomerModule } from './customer/customer.module';
+import { MynameController } from './myname/myname.controller';
 
 @Module({
   imports: [StudentModule, CustomerModule],
-  controllers: [AppController],
+  controllers: [AppController, MynameController],
   providers: [AppService],
 })
 export class AppModule {}
